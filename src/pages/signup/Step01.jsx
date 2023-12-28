3c61f7d877960ab0521d9741ddd9da232b7a3486
Mohammad Ravoof <140537466+mohammadravoof@users.noreply.github.com>
2023-12-28 22:39:42 +0530

    Update Step01.jsx

--- src/pages/signup/Step01.jsx
+++ src/pages/signup/Step01.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import Button from '../../components/ui/Button'
-import Fieldset from '../../components/ui/Fieldset'
-import Steps from '../../components/ui/Steps'
-import Dobfetcher from '../../components/ui/Dobfetcher'
-import HeadingOfCreateAccout from '../../components/ui/HeadingOfCreateAccout'
+import React, { useState }from 'react';
+import Button from '../../components/ui/Button';
+import Fieldset from '../../components/ui/Fieldset';
+import Steps from '../../components/ui/Steps';
+import Dobfetcher from '../../components/ui/Dobfetcher';
+import HeadingOfCreateAccout from '../../components/ui/HeadingOfCreateAccout';
 import { useNavigate } from "react-router-dom";
 
-function Step1() {
+const Step1 = ( ) => {
+
+  
+
   const navigate = useNavigate();
   return (
 
@@ -16,7 +19,7 @@ function Step1() {
     <Steps onClick={() => navigate("/")}>1</Steps>
     <HeadingOfCreateAccout text="Create your account"></HeadingOfCreateAccout> 
     <div className="flex flex-col items-center gap-8 self-stretch">
-    <Fieldset text="Name" input="Name"/>
+    <Fieldset text="Name" input="Name" />
     <Fieldset text="Email" input="Email"/>
       <div className="flex flex-col items-start gap-2 self-stretch">
         <div className="text-neutral-50 text-[15px] font-bold">Date of birth</div>
@@ -26,7 +29,7 @@ function Step1() {
 
     </div>
     <div className="flex py-20 px-5 flex-col justify-end items-center gap-[10px] self-stretch grow shrink-0 basis-0">
-    <Button variant="white" onClick={() => navigate("/Step2")}>Create account</Button>
+    <Button variant="white" onClick={() => navigate("/Step2")}  >Create account</Button>
     </div>
   </div>
 </div>
